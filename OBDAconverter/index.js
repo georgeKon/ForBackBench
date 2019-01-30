7a0c2db578f824be7f0e30f011a754b12e9fb82b
James Errington <james@maprunner.co.uk>
2019-01-30 16:54:54 +0000

    Add Sparql converter

--- OBDAconverter/index.js
+++ OBDAconverter/index.js
@@ -13,6 +13,12 @@ program
   .description('Convert ChaseBench common format schema to SQL')
   .action((schema, options) => convertSchemaToSqlCmd(schema, options))
 
+program
+  .command('sparql')
+  .arguments('<query>')
+  .description('Convert SPARQL query to conjunctive query')
+  .action((query, options) => convertSparqlToQueryCmd(query))
+
 program.parse(process.argv)
 
 module.exports = {
