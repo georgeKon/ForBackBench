7bc7e91e52fbca4deb40d291f19f1c7334a3157f
James Errington <james@maprunner.co.uk>
2019-03-07 11:32:27 +0000

    Fix scenarios

--- scenarios/LUBM/R22.sql
+++
@@ -1,7 +0,0 @@
-SELECT A.c0, A.c1
-FROM "headOf" as A
-
-UNION
-SELECT A.c0, A.c1
-FROM "worksFor" as A
-;
--- scenarios/LUBM/test.sql
+++
@@ -1,2 +0,0 @@
-CREATE TABLE "AdministrativeStaff" (c0 text);
-CREATE TABLE "age" (c0 text, c1 text);
