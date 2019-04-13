8c506076f42e1c826437acad47e4e25b24562a64
James Errington <james@maprunner.co.uk>
2019-04-13 16:30:24 +0100

    test chasestepper on scenarios

--- run.sh
+++ run.sh
@@ -46,6 +46,13 @@ echo "Converting: $((($ENDTIME - $STARTTIME)/1000000)) milliseconds"
 
 psql -h $PGHOST -p $PGPORT -U $PGUSER -d $PGDATABASE -c "SELECT COUNT(*) FROM (${SQL%?}) AS query;"
 
+echo "===== ChaseStepper ====="
+STARTTIME=$(date +%s%N)
+./tools/chasestepper/run.sh $1/schema/s-schema.txt $1/schema/t-schema.txt $1/dependencies/st-tgds.txt $1/data $1/dependencies/t-tgds.txt $1/cs-out/ $1/queries/RDFox/Q$2/ $2 $1/cs-results | grep "Query" -A 1
+ENDTIME=$(date +%s%N)
+echo "Time elapsed: $((($ENDTIME - $STARTTIME)/1000000)) milliseconds"
+
+
 echo "===== RDFox ====="
 STARTTIME=$(date +%s%N)
 java -jar tools/RDFox/chaseRDFox-linux.jar -chase standard -s-sch $1/schema/s-schema.txt -t-sch $1/schema/t-schema.txt -st-tgds $1/dependencies/st-tgds.txt -src $1/data -t-tgds $1/dependencies/t-tgds.txt -trg $1/out/ -qdir $1/queries/RDFox/Q$2/ -qres $1/results | grep "Query" -A 1
