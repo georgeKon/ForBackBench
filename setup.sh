da6e1887bd3e58ce70e117b7a40be63fb3e36681
James Errington <james@maprunner.co.uk>
2019-05-11 14:00:51 +0100

    fix setup.sh for test

--- setup.sh
+++ setup.sh
@@ -1,5 +1,3 @@
-./scripts/bootstrap.sh scenarios/A dllite data
-./scripts/bootstrap.sh scenarios/S dllite data
 ./scripts/bootstrap.sh scenarios/V dllite data
 ./scripts/bootstrap.sh scenarios/U chasebench
 
@@ -7,4 +5,3 @@ cd scenarios/U/data/small && tar -xf small.tar.xz && rm small.tar.xz &&\
 cd ../medium && tar -xf medium.tar.xz && rm medium.tar.xz &&\
 cd ../large && tar -xf large.tar.xz && rm large.tar.xz &&\
 cd ../huge && tar -xf small.tar.xz && rm small.tar.xz
-  
