75e8c0cf143f431f3f771ad174c6d6c39a6a76be
Unknown <jhhfrjou1@gmail.com>
2019-07-19 12:23:24 +0100

    updated tools with new jars

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mkdir -p $1/data/$2
-java -jar tools/datagenerator/datagenerator-1.0.jar $1/schema/s-schema.txt $1/data/$2 tools/datagenerator/config.ini $2
+java -jar tools/datagenerator/datagenerator-1.01.jar $1/schema/s-schema.txt $1/data/$2 tools/datagenerator/config.ini $2
