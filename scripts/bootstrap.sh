25aaf640e055435c9a6ed62b735e3dd3dff443bf
Tom Blount <tom@tomblount.co.uk>
2021-04-19 09:15:50 +0100

    Updated bootstrap to reference CreateCGQRTTGDs.jar and CreateLAVandGAV.jar; needs confirming that these refs are in the correct place

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -47,9 +47,18 @@ then
 	mv $BASE_DIR/schema/t-schema.sql $BASE_DIR/schema/oneToOne/t-schema.sql
 
 
-# 	java -jar utilityTools/schemaGenerator-1.08.jar -st-tgds $BASE_DIR/dependencies/oneToOne-st-tgds.txt -t-tgds $BASE_DIR/dependencies/oneToOne-t-tgds.txt -s-schema $BASE_DIR/schema/oneToOne/s-schema.txt -t-schema $BASE_DIR/schema/oneToOne/t-schema.txt
-   # TODO: create GAV and LAV Schema
+  # java -jar utilityTools/schemaGenerator-1.08.jar -st-tgds $BASE_DIR/dependencies/oneToOne-st-tgds.txt -t-tgds $BASE_DIR/dependencies/oneToOne-t-tgds.txt -s-schema $BASE_DIR/schema/oneToOne/s-schema.txt -t-schema $BASE_DIR/schema/oneToOne/t-schema.txt
+  # TODO: create GAV and LAV Schema
 	
+  # TODO: confirm this is the correct place to be creating it
+  if [[ $4 = "GAV" ]]; then
+    java -jar utilityTools/CreateLAVandGAV.jar --create GAV
+  fi
+  if [[ $4 = "LAV" ]]; then
+    java -jar utilityTools/CreateLAVandGAV.jar --create LAV
+  fi
+
+
   if [[ $3 = "data" ]]; then
     for size in ${SIZES[*]}; do
       mkdir -p $BASE_DIR/data/$GAVorLAV/$size
@@ -63,6 +72,9 @@ java -jar utilityTools/ontopMappingGenerator-1.08.jar -st-tgds $BASE_DIR/depende
 
 mkdir -p $BASE_DIR/CGQRFiles
 # TODO: create t-tgd.txt and st-tgd.txt from jar file (that Tom has) and place it inside CGQRFiles
+# TODO: this currently only creates a t-tgd file;
+# TODO: confirm output location; currently goes to <scenario>/dependencies/cgqr-t-tgds.txt
+java -jar utilityTools/CreateCGQRTTGDs.jar
 
 mkdir -p $BASE_DIR/rulewerkfiles/GAV
 mkdir -p $BASE_DIR/rulewerkfiles/LAV
